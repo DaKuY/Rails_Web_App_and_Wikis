0e85cde9292c117b6ba34909b7a48ffef21037f6
Andrew Camero (w) <drewe927@gmail.com>
2014-04-25 15:20:31 -0500

    Added basic security for deleting comments and posts

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,8 +1,18 @@
 class CommentsController < ApplicationController
+  
+    http_basic_authenticate_with name: "admin", password: "adminSecret876*", only: :destroy
+  
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
+  end
+  
+  def destroy
+    @post = Post.find(params[:post_id])
+	@comment = @post.comments.find(params[:id])
+	@comment.destroy
+	redirect_to post_path(@post)
   end
  
   private
