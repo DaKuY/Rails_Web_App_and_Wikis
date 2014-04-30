8305d68c31279ab10cce42a314a71cc553146a2f
Andrew Camero (w) <drewe927@gmail.com>
2014-04-30 10:29:59 -0500

    Finished Exercise 1 w/ extra credit

--- LearnRubyTheHardWay/ex1.rb
+++ LearnRubyTheHardWay/ex1.rb
@@ -5,3 +5,4 @@ puts "This is fun."
 puts 'Yay! Printing.'
 puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.'
+puts "\nExtra Credit\n" + "1. Print another line\n" + "2. Prints only one line (this is one line just separated by 'backslash n'\n" + "3. # comments out code"
