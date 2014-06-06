ae0a4de34dc74ad9793c0bae0c9749a118fe7a73
Andrew Camero (w) <drewe927@gmail.com>
2014-06-06 07:54:44 -0500

    Exercises 11-14 complete

---
+++ LearnRubyTheHardWay/ex11.rb
@@ -0,0 +1,11 @@
+# Exercise 11: Asking Questions
+
+#print prints with out a new line, puts prints with a \n
+print "How old are you? "
+age = gets.chomp()
+print "How tall are you? "
+height = gets.chomp()
+print "How much do you weigh? "
+weight = gets.chomp()
+
+puts "So, you're #{age} old, #{height} tall, and #{weight} heavy."
