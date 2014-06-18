dc367a0e7816b283a37439f4bb48f44b4f95b889
Andrew Camero (w) <drewe927@gmail.com>
2014-06-18 08:31:06 -0500

    Exercise 17.rb half-way complete

---
+++ LearnRubyTheHardWay/ex17.rb
@@ -0,0 +1,10 @@
+# Exercise 17: More Files
+
+from_file, to_file = ARGV
+script = $0
+
+puts "Copying from #{from_file} to #{to_file}"
+
+# we could do these two on one line too, how?
+input = File.open(from_file)
+indata = input.read()
